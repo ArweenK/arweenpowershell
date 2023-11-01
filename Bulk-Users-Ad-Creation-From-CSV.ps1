69f2e587f24ce5b5246889e49d1f4d28aa27fc6e
ArweenK <120675175+ArweenK@users.noreply.github.com>
2023-11-01 15:14:40 -0700

    Create Bulk-Users-Ad-Creation-From-CSV.ps1

---
+++ Bulk-Users-Ad-Creation-From-CSV.ps1
@@ -0,0 +1,28 @@
+﻿#Creating bulk users from a .csv to the "Email Only" OU
+
+
+
+$ADUsers = Import-csv -Path "c:\users.csv"
+
+foreach ($User in $ADUsers) {
+
+    $Username    = $User.username
+    $Password    = $User.password
+    $Firstname   = $User.firstname
+    $Lastname    = $User.lastname
+    $Department  = $User.department
+    $OU          = $User.ou
+
+    New-ADUser `
+        -SamAccountName $Username `
+        -UserPrincipalName "$Username@kavousi.com" `
+        -Name "$Firstname $Lastname" `
+        -GivenName $Firstname `
+        -Surname $Lastname `
+        -Enabled $True `
+        -ChangePasswordAtLogon $False `
+        -DisplayName "$Lastname, $Firstname" `
+        -Department $Department `
+        -Path $OU `
+        -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force)
+}
